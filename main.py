521a87cf4ec80253cf0fdf01a8c81e33ddf28765
Tarold <ltdecorer@gmail.com>
2023-12-21 23:58:20 +0200

    Add 3 ex to 5 tab

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk, messagebox
 import subprocess
 from PIL import Image, ImageTk
 
+
 class MainApplication(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -15,11 +16,8 @@ class MainApplication(tk.Tk):
 
         self.tabControl = ttk.Notebook(self)
 
-        self.create_tab_with_tasks("1 ЛР", [1, 2], ["1.1.png", "1.2.png"])
-        self.create_tab_with_tasks("2 ЛР", [1, 2], ["2.1.png", "2.2.png"])
-        # Add other tabs as needed
-
-        self.show_main_content("Самостоятельная работа №1\nСтудент: КНТ113СП Олексій Сімічов")
+        self.show_main_content(
+            "Самостоятельная работа №1\nСтудент: КНТ113СП Олексій Сімічов")
 
     def create_menu(self):
         menu_bar = tk.Menu(self)
@@ -27,23 +25,14 @@ class MainApplication(tk.Tk):
         for i in range(1, 7):
             sub_menu = tk.Menu(menu_bar, tearoff=0)
             menu_bar.add_cascade(label=f"{i} ЛР", menu=sub_menu)
-            for t in [1, 2]:
+            for t in [1, 2] if (i != 5) else [1, 2, 3]:
                 img = f"{i}.{t}.png"
-                sub_menu.add_command(label=f"Задание {t}", command=lambda i=i, t=t: self.run_task(i, t))
-                sub_menu.add_command(label=f"Показать фото {t}", command=lambda img=img: self.show_image(img))
+                sub_menu.add_command(
+                    label=f"Задание {t}", command=lambda i=i, t=t: self.run_task(i, t))
+                sub_menu.add_command(
+                    label=f"Показать фото {t}", command=lambda img=img: self.show_image(img))
 
         self.config(menu=menu_bar)
-
-    def create_tab_with_tasks(self, title, tasks, image_paths):
-        tab = ttk.Frame(self.tabControl)
-        self.tabControl.add(tab, text=title)
-
-        task_listbox = tk.Listbox(tab)
-        for task in tasks:
-            task_listbox.insert(tk.END, f"Задание {task}")
-
-        task_listbox.bind("<ButtonRelease-1>", lambda event, img_paths=image_paths: self.on_task_click(event, img_paths))
-        task_listbox.pack(padx=10, pady=10)
 
     def run_task(self, lab_number, task_number):
         script_path = f"{lab_number}.{task_number}.py"
@@ -53,12 +42,6 @@ class MainApplication(tk.Tk):
             return
 
         subprocess.run(["python3", script_path])
-
-    def on_task_click(self, event, image_paths):
-        selected_task = event.widget.get(event.widget.curselection())
-        lab_number, task_number = selected_task.split()[1].split('.')
-        img_path = image_paths[int(task_number) - 1]
-        self.run_task(int(lab_number), int(task_number), img_path)
 
     def show_image(self, image_path):
         image = Image.open(image_path)
@@ -78,10 +61,12 @@ class MainApplication(tk.Tk):
         label = tk.Label(main_frame, text=content, padx=10, pady=10)
         label.pack()
 
-        self.tabControl.pack(in_=main_frame, side="top", fill="both", expand=True)
+        self.tabControl.pack(in_=main_frame, side="top",
+                             fill="both", expand=True)
 
     def run(self):
         self.mainloop()
+
 
 if __name__ == "__main__":
     app = MainApplication()
